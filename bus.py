595d181b9e0b83579d5216ef1009e52fc7af09ca
benedit quezada <beneditquezada@outlook.com>
2026-03-02 15:21:21 -0500

    error numero from numeros

--- bus.py
+++ bus.py
@@ -35,7 +35,7 @@ print(f'lista original : {numeros}')
 numeros[0] =111
 print(f' \n lista actualizada = {numeros}')
 
-numero[1] = numeros[4]
+numeros[1] = numeros[4]
 print(f' \n lista nueva es : {numeros}')
 
 print(f' \n la longitud de la lista es : {len(numeros)}')
