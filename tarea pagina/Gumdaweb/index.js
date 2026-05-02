d23be3c5e337940cf4c4dc732edde06ecb4d5ada
benedit quezada <beneditquezada@outlook.com>
2026-05-01 22:36:26 -0400

    fixed

--- tarea pagina/Gumdaweb/index.js
+++ tarea pagina/Gumdaweb/index.js
@@ -1,1 +1,8 @@
-console.log(); 
+const images = document.querySelectorAll(".gallery img");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    images.forEach(i => i.style.width = "10%");
+    img.style.width = "40%";
+  });
+});
