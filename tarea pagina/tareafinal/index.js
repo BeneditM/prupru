2bd41bc85fc6888f391618d091c0687d9831ed21
benedit quezada <beneditquezada@outlook.com>
2026-05-11 22:36:31 -0400

    script

--- tarea pagina/tareafinal/index.js
+++ tarea pagina/tareafinal/index.js
@@ -1,27 +1,29 @@
 
 
 let contactos = [ {
-    nombre: "Maxwell wright",
+    nombre: "pedro martinez",
     telefono: "809-399-6574",
-    email: "maxwr@gmail.com"
+    email: "pmartinez@gmail.com"
 }, {
-    nombre: "Juan Suero",
+    nombre: "Juan soto",
     telefono: "765-980-3243",
-    email: "juans@gmail.com"
+    email: "juanmvpsoto@gmail.com"
 }, {
-    nombre: "maria smith",
+    nombre: "anthonio spallath",
     telefono: "984-735-0909",
-    email: "maria_smith@gmail.com"
+    email: "anthoniokillpleope@gmail.com"
 } ];
-    contactos.push ( {
-        nombre: "masiel hernandez",
-        telefono: "645-023-6549",
-        email: "masielh@gmail.com"
 
-    } );
 
-    let ultimo = contactos.length -1;
-    
-    console.log (`${contactos[0].nombre} / ${contactos[0].telefono} / ${contactos[0].email}`);
+contactos.push({
+    nombre : prompt("Ingresa el nombre"),
+    telefono : prompt("Ingresa el telefono"),
+    email : prompt("Ingrese el email")
+});
 
-    console.log (`${contactos[ultimo].nombre} / ${contactos[ultimo].telefono} / ${contactos[ultimo].email}`);
+let ultimo = contactos.length  - 1;
+
+console.log (`${contactos[0].nombre} // ${contactos[0].telefono} // ${contactos[0].email}`);
+
+console.log (`${contactos[ultimo].nombre} // ${contactos[ultimo].telefono} // ${contactos[ultimo].email}`);
+
